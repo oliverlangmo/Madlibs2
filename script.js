ba9ebc719a68ac5e7f279a566c523dc040c1e49a
oliverlangmo <oliver.langmo@gmail.com>
2015-07-04 08:50:03 -0500

    refactor

---
+++ script.js
@@ -0,0 +1,30 @@
+var thisApp = angular.module('demo', [])
+
+thisApp.controller('demoCtrl', function($scope) {
+	$scope.girlboy = 'girl';
+	$scope.heshe = 'she';
+	$scope.character_name = '';
+	$scope.adjective = '';
+	$scope.time_of_day='';
+	$scope.destination='';
+	$scope.location1='';
+	$scope.time_of_day='';
+	$scope.destination2='';
+	$scope.occupation='';
+	$scope.adjective2='';
+    $scope.alcohol='';
+    $scope.adjective3='';  
+
+
+$scope.male=function(){
+  $scope.girlboy="boy";
+  $scope.heshe="he";
+};
+$scope.female = function () { 
+	$scope.girlboy = 'girl';  
+    $scope.heshe = "she";
+};
+
+
+
+})
