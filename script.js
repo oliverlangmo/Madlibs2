8b942e844663a49ce3ccb0fd94d0764efa08a305
oliverlangmo <oliver.langmo@gmail.com>
2015-07-06 18:34:46 -0500

    w title

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 var thisApp = angular.module('demo', [])
 
-thisApp.controller('demoCtrl', function($scope) {
-	$scope.girlboy = 'girl';
-	$scope.heshe = 'she';
-	$scope.character_name = '';
-	$scope.adjective = '';
-	$scope.time_of_day='';
-	$scope.destination='';
-	$scope.location1='';
-	$scope.time_of_day='';
-	$scope.destination2='';
-	$scope.occupation='';
-	$scope.adjective2='';
-    $scope.alcohol='';
-    $scope.adjective3='';  
-
-
-$scope.male=function(){
-  $scope.girlboy="boy";
-  $scope.heshe="he";
-};
-$scope.female = function () { 
-	$scope.girlboy = 'girl';  
-    $scope.heshe = "she";
-};
 
 
 
-})
+thisApp.controller('demoCtrl', ['$scope', function($scope){
+	 $scope.inputVisable = 'visable';
+  $scope.outputVisable  = '';
+  $scope.gender = '';
+
+  $scope.submit = function(){
+    if( $scope.userSubmit.$valid ) {
+      $scope.switch();
+    } else {
+      alert("Please fill out all of the forms");
+    }
+  }
+  $scope.male = function(){
+    $scope.heshe = 'he';
+    $scope.girlboy = 'boy';
+  };
+  $scope.female = function(){
+    $scope.heshe = 'she';
+    $scope.girlboy = 'girl';
+    
+  };
+  $scope.switch = function(){
+    if ($scope.inputVisable){
+      $scope.inputVisable = '';
+      $scope.outputVisable = 'visable';
+    } else {
+      $scope.inputVisable = 'visable';
+      $scope.outputVisable = '';
+    }
+  };
+  $scope.reset = function(){
+    $scope.switch();
+    $scope.data.handle = '';
+    $scope.data.adjective = '';
+    $scope.data.when = '';
+    $scope.data.where = '';
+    $scope.data.place = '';
+    $scope.data.when = '';
+    $scope.data.where2 = '';
+    $scope.data.job = '';
+    $scope.data.adjective2 = '';
+    $scope.data.alcohol = '';
+    $scope.data.adjective3 = '';
+  };
+}]);
